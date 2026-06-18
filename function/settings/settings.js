26da7a5bc6651a880bcd6a367517a97e3fc9202a
chamldamar9-wq <chamldamar9@gmail.com>
2026-06-18 15:37:30 +0100

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -15,7 +15,7 @@ global.setting = {
 
 global.info = {
  nomerbot : '212605784394',
- pairingNumber : '212624854739',
+ pairingNumber : '212702816550',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
